fed02f05fcf71aa0946ebe7f7c3988bc5f104059
Adnan Azem <adnan.azem.250@gmail.com>
2024-01-08 14:17:36 +0200

    add permissions

--- lib/View/second_exam/end_second_test_screen.dart
+++ lib/View/second_exam/end_second_test_screen.dart
@@ -28,74 +28,76 @@ class _EndOfSecondTest extends State<EndOfSecondTest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue[600]?.withOpacity(0.5),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[600]?.withOpacity(0.5),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background1.png"),
+              fit: BoxFit.cover),
         ),
-        body: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/background1.png"),
-                  fit: BoxFit.cover),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 300),
+            const Center(
+              child: Text(
+                "You finished the second part",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Alkatra'),
+              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 300),
-                const Center(
+            const SizedBox(height: 80),
+            TextButton(
+              onPressed: () {
+                createFolder();
+                createInfo();
+                createFirstExam();
+                createSecondExam();
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //       builder: (context) => const ThirdExplanationPage()),
+                // );
+              },
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.blue[300]?.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      offset: const Offset(
+                        7.0,
+                        7.0,
+                      ),
+                      blurRadius: 10.0,
+                    ),
+                  ],
+                ),
+                child: const Center(
                   child: Text(
-                    "You finished the second part",
+                    'next part',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         color: Colors.black,
-                        fontSize: 60,
+                        fontSize: 30,
                         fontWeight: FontWeight.bold,
                         fontFamily: 'Alkatra'),
                   ),
                 ),
-                const SizedBox(height: 80),
-                TextButton(
-                  onPressed: () {
-                    createFolder();
-                    createInfo();
-                    createFirstExam();
-                    createSecondExam();
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //       builder: (context) => const ThirdExplanationPage()),
-                    // );
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      color: Colors.blue[300]?.withOpacity(0.8),
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          offset: const Offset(
-                            7.0,
-                            7.0,
-                          ),
-                          blurRadius: 10.0,
-                        ),
-                      ],
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'next part',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Alkatra'),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )));
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
